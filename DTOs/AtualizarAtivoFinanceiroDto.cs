edbcd14f74668e5355c883a5cf144534321143e8
josiasdev <francisco.batista67@alu.ufc.br>
2025-07-16 13:34:40 -0300

    feat: Criando um dto responsavel para atualizar os ativos financeiros

--- DTOs/AtualizarAtivoFinanceiroDto.cs
+++ DTOs/AtualizarAtivoFinanceiroDto.cs
@@ -1,6 +1,19 @@
-namespace InvestTrack.API.DTOs;
 
-public class AtualizarAtivoFinanceiroDto
+using System.ComponentModel.DataAnnotations;
+
+namespace InvestTrack.API.DTOs
 {
-    
+    public class AtualizarAtivoFinanceiroDto
+    {
+        [Required]
+        public string Ticker { get; set; }
+
+        [Required]
+        public string NomeEmpresa { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantidade { get; set; }
+
+        public decimal PrecoMedioCompra { get; set; }
+    }
 }
