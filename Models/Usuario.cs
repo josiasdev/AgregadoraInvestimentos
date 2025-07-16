e1cbd37aace66aa21ed8ba5f14f52c9d46e85e50
josiasdev <francisco.batista67@alu.ufc.br>
2025-07-16 09:58:38 -0300

    feat: Criando modelo de usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -2,5 +2,8 @@ namespace InvestTrack.API.Models;
 
 public class Usuario
 {
-    
+    public int Id { get; set; }
+    public string NomeCompleto { get; set; }
+    public string Email { get; set; }
+    public List<AtivoFinanceiro> Portfolio { get; set; } = new List<AtivoFinanceiro>();
 }
