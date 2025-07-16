ba3e2233da0cb18cad57bca6a6f853da1e62d97a
josiasdev <francisco.batista67@alu.ufc.br>
2025-07-16 13:31:03 -0300

    feat: Criar um dto responsavel para criar um novo ativo financeiro

--- DTOs/CriarAtivoFinanceiroDto.cs
+++ DTOs/CriarAtivoFinanceiroDto.cs
@@ -1,6 +1,21 @@
-namespace InvestTrack.API.DTOs;
+using System.ComponentModel.DataAnnotations;
 
-public class CriarAtivoFinanceiroDto
+namespace InvestTrack.API.DTOs
 {
-    
+    public class CriarAtivoFinanceiroDto
+    {
+        [Required]
+        public string Ticker { get; set; }
+
+        [Required]
+        public string NomeEmpresa { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantidade { get; set; }
+
+        public decimal PrecoMedioCompra { get; set; }
+
+        [Required]
+        public int UsuarioId { get; set; }
+    }
 }
