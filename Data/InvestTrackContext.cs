7e20d8b29fc63149da897d547b17a1b4258d053a
josiasdev <francisco.batista67@alu.ufc.br>
2025-07-16 10:10:41 -0300

    feat: Criar uma ponte do codigo c# com o banco de dados sql server.

--- Data/InvestTrackContext.cs
+++ Data/InvestTrackContext.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using InvestTrack.API.Models;
+
 namespace InvestTrack.API.Data;
 
-public class InvestTrackContext
+public class InvestTrackContext : DbContext
 {
+    public InvestTrackContext(DbContextOptions<InvestTrackContext> options) : base(options) { }
     
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<AtivoFinanceiro> AtivosFinanceiros { get; set; }
 }
