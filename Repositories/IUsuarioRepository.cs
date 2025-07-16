48c002b22184e3b46df47f5626759ce7501bee7f
josiasdev <francisco.batista67@alu.ufc.br>
2025-07-16 10:26:11 -0300

    feat: interface de usuario

--- Repositories/IUsuarioRepository.cs
+++ Repositories/IUsuarioRepository.cs
@@ -1,6 +1,11 @@
+using InvestTrack.API.Models;
 namespace InvestTrack.API.Repositories;
 
 public interface IUsuarioRepository
 {
-    
+    Task<Usuario> GetByIdAsync(int id);
+    Task<IEnumerable<Usuario>> GetAllAsync();
+    Task AddAsync(Usuario usuario);
+    Task UpdateAsync(Usuario usuario);
+    Task DeleteAsync(int id);
 }
