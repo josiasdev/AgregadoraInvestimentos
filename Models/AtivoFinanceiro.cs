3a3fff2561dfa47b6b4d346a708230325057afd3
josiasdev <francisco.batista67@alu.ufc.br>
2025-07-16 10:03:09 -0300

    feat: Criando modelo de AtivoFinanceiro

--- Models/AtivoFinanceiro.cs
+++ Models/AtivoFinanceiro.cs
@@ -2,5 +2,11 @@ namespace InvestTrack.API.Models;
 
 public class AtivoFinanceiro
 {
-    
+    public int Id { get; set; }
+    public string Ticker { get; set; } // Ex: "PETR4", "MXRF11"
+    public string NomeEmpresa { get; set; }
+    public int Quantidade { get; set; }
+    public decimal PrecoMedioCompra { get; set; }
+    public int UsuarioId { get; set; } // Chave estrangeira
+    public Usuario Usuario { get; set; }
 }
